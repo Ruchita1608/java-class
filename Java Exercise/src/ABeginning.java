28d499fa044b1cca96bb2bf9325bb363fd6da588
Ruchita <rushah@wiley.com>
2024-04-18 12:50:29 +0100

    this is a message

---
+++ Java Exercise/src/ABeginning.java
@@ -0,0 +1,9 @@
+public class ABeginning {
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World");
+        System.out.println("First java Program");
+        
+    }
+}
